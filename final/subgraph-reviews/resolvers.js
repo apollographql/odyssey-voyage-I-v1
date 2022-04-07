5849f464074a0113ece9a0f5667e2fedbe55c477
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2022-04-07 06:29:40 -0600

    fix(starter-code): starter code updates

--- final/subgraph-reviews/resolvers.js
+++ final/subgraph-reviews/resolvers.js
@@ -1,24 +1,24 @@
 const resolvers = {
   Query: {
-    latestReviews(_, __, {dataSources}) {
+    latestReviews: (_, __, {dataSources}) => {
       return dataSources.reviewsAPI.getLatestReviews();
     }
   },
   Review: {
-    location({locationId}) {
+    location: ({locationId}) => {
       return {id: locationId};
     }
   },
   Location: {
-    overallRating({id}, _, {dataSources}) {
+    overallRating: ({id}, _, {dataSources}) => {
       return dataSources.reviewsAPI.getOverallRatingForLocation(id);
     },
-    reviews({id}, _, {dataSources}) {
+    reviews: ({id}, _, {dataSources}) => {
       return dataSources.reviewsAPI.getReviewsForLocation(id);
     }
   },
   Mutation: {
-    submitReview(_, {review}, {dataSources}) {
+    submitReview: (_, {review}, {dataSources}) => {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
       return {code: 200, success: true, message: 'success', review: newReview};
     }
